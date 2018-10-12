06175ad8dbb39a7280a972427b9175daf39f8fb6
Bojie Wu <bojie@dexon.org>
2018-10-12 13:50:17 +0800

    app: implement new interface method

--- core/blockchain.go
+++ core/blockchain.go
@@ -18,6 +18,7 @@
 package core
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -26,6 +27,9 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	coreCommon "github.com/dexon-foundation/dexon-consensus-core/common"
+	coreTypes "github.com/dexon-foundation/dexon-consensus-core/core/types"
 
 	"github.com/dexon-foundation/dexon/common"
 	"github.com/dexon-foundation/dexon/common/mclock"
@@ -129,6 +133,10 @@ type BlockChain struct {
 	vmConfig  vm.Config
 
 	badBlocks *lru.Cache // Bad block cache
+
+	confirmedBlockMu       sync.Mutex
+	confirmedBlock         map[coreCommon.Hash]*coreTypes.Block
+	filteredConfirmedBlock map[uint32]map[coreCommon.Hash]*coreTypes.Block
 }
 
 // NewBlockChain returns a fully initialised block chain using information
@@ -197,6 +205,54 @@ func NewBlockChain(db ethdb.Database, cacheConfig *CacheConfig, chainConfig *par
 
 func (bc *BlockChain) getProcInterrupt() bool {
 	return atomic.LoadInt32(&bc.procInterrupt) == 1
+}
+
+func (bc *BlockChain) AddConfirmedBlock(block *coreTypes.Block) {
+	bc.confirmedBlockMu.Lock()
+	defer bc.confirmedBlockMu.Unlock()
+
+	bc.confirmedBlock[block.Hash] = block
+	bc.filteredConfirmedBlock[block.Position.ChainID][block.Hash] = block
+}
+
+func (bc *BlockChain) RemoveConfirmedBlock(hash coreCommon.Hash) {
+	bc.confirmedBlockMu.Lock()
+	defer bc.confirmedBlockMu.Unlock()
+
+	block := bc.confirmedBlock[hash]
+	delete(bc.filteredConfirmedBlock[block.Position.ChainID], block.Hash)
+	delete(bc.confirmedBlock, block.Hash)
+}
+
+func (bc *BlockChain) GetConfirmedBlockByHash(hash coreCommon.Hash) *coreTypes.Block {
+	return bc.confirmedBlock[hash]
+}
+
+func (bc *BlockChain) GetNonceInConfirmedBlock(chainID uint32, address common.Address) (uint64, bool, error) {
+	var nonce uint64
+	var init bool
+	for _, block := range bc.filteredConfirmedBlock[chainID] {
+		var transactions types.Transactions
+		err := rlp.Decode(bytes.NewReader(block.Payload), &transactions)
+		if err != nil {
+			return 0, init, err
+		}
+
+		for _, tx := range transactions {
+			msg, err := tx.AsMessage(types.MakeSigner(bc.chainConfig, new(big.Int)))
+			if err != nil {
+				return 0, init, err
+			}
+
+			if msg.From() == address && (tx.Nonce() > nonce || !init) {
+				if !init {
+					init = true
+				}
+				nonce = tx.Nonce()
+			}
+		}
+	}
+	return nonce, init, nil
 }
 
 // loadLastState loads the last known chain state from the database. This method
