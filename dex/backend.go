c20b2c71730cccb31c32ebfe1da037b0747d1777
Sonic <sonic@dexon.org>
2018-12-05 15:17:38 +0800

    core, dex: polish sync (#75)
    - Broadcasting blocks at chain head event is not correct when the full
      node is not running in block proposer mode. Introduce NewFinalizedBlockEvent,
      this event is post by the full node which runs in block proposer mode when a
      block is witnessed and resulting in some blocks are considered finalized.
    
    - Non block proposer node will still broadcast blocks
      at the following moment (same as ethereum):
      1. a sync with a peer is terminated successfully
      2. a block passes the fetcher's header check during inserting blocks
      3. a block is successfully inserted by fetcher
    
    - Don't trigger a sync when we are not behind other peers more than
      acceptable distance. Fetcher is able to cover this.

--- dex/backend.go
+++ dex/backend.go
@@ -155,7 +155,7 @@ func New(ctx *node.ServiceContext, config *Config) (*Dexon, error) {
 
 	pm, err := NewProtocolManager(dex.chainConfig, config.SyncMode,
 		config.NetworkId, dex.eventMux, dex.txPool, dex.engine, dex.blockchain,
-		chainDb, config.BlockProposerEnabled, dex.governance)
+		chainDb, config.BlockProposerEnabled, dex.governance, dex.app)
 	if err != nil {
 		return nil, err
 	}
@@ -243,6 +243,7 @@ func (s *Dexon) Start(srvr *p2p.Server) error {
 }
 
 func (s *Dexon) Stop() error {
+	s.app.Stop()
 	return nil
 }
 
